25271c5c0107794bb956a92b75c666ed1d64844d
Anthony Carton <mr.carton@gmail.com>
2018-05-15 15:51:38 -0600

    Start somewhere in progress, got Atom to work.

---
+++ js-process/start-somewhere-01.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+
+
+// fuelCost() algorithm
+// should be able to calc gas, electricity, E85, CNG, LPG, and diesel/B20/B100
+function fuelCost() {
+	// scope escalation rate / year constants locally
+	// escalation rates from: afdc.energy.gov/calc/cost_calculator_methodology.html#sources
+	const escGas = 0.018;
+	const escE85 = 0.016;
+	const escCng = 0.003;
+	const escLpg = 0.013;
+	const escDie = 0.018;
+	const escB20 = 0.018;
+	const escB100 = 0.018;
+
+	// electricty has negative escalation rate
+	const escEle = -0.003;
+	afodinafsd
+
+}
